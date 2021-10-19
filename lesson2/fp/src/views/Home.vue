77e0d184795c028bb961a8da6c8b17f3e0777c88
Alex <anflygel@gmail.com>
2021-10-19 19:12:23 +0400

    calculate-v-for

--- lesson2/fp/src/views/Home.vue
+++ lesson2/fp/src/views/Home.vue
@@ -5,25 +5,27 @@
     <input v-model.number="operand2" type="number" @input="notDivide($event)" />
     = {{ result }}
     <br />
-    <button @click="result = operand1 + operand2">+</button>
-    <button @click="minus(operand1, operand2)">-</button>
-    <button @click="multiplication">*</button>
-    <button v-bind:disabled="isButtonDisabled" @click="divide">/</button>
+
+    <button
+      v-for="operand in operands"
+      v-bind:key="operand"
+      v-bind:title="operand"
+      v-bind:disabled="operand1 === '' || operand2 === ''"
+      @click="calculate(operand)"
+    >
+      {{ operand }}
+    </button>
+    <div v-if="error">Ошибка! {{ error }}</div>
+    <div class="logs">
+      <div v-for="(log, id) in logs" v-bind:key="id">{{ log }}</div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-
-//Первая попытка убрать деление на ноль
-
-// let notnull = false;
-// let twonumber = document.getElementById("second");
-// if (twonumber.target.value == 0) {  //ошибок нет, сайт не открывается почему-то
-//   notnull = true;
-// }
-// console.log(twonumber);
+import Vue from "vue";
 
 export default {
   name: "Home",
@@ -32,27 +34,58 @@ export default {
   },
   data() {
     return {
+      operands: ["+", "-", "*", "/"],
       operand1: 0,
       operand2: 0,
       result: 0,
       isButtonDisabled: true,
+      error: "",
+      logs: {}, //здесь будем хранить наши логи
     };
   },
   methods: {
-    minus(op1, op2) {
-      this.result = op1 - op2;
+    add() {
+      this.result = this.operand1 + this.operand2;
+    },
+    minus() {
+      this.result = this.operand1 - this.operand2;
     },
     multiplication() {
       this.result = this.operand1 * this.operand2;
     },
     divide() {
-      this.result = this.operand1 / this.operand2;
+      const { operand1, operand2 } = this;
+      if (operand2 === 0) {
+        this.error = "Делить на 0 нельзя";
+      } else {
+        this.result = operand1 / operand2;
+      }
+
+      // this.result = this.operand1 / this.operand2;
     },
-    //метод отмены деления на ноль
-    notDivide(event) {
-      if (event.target.value == 0) {
-        this.isButtonDisabled = true;
-      } else this.isButtonDisabled = false;
+
+    calculate(operation = "+") {
+      this.error = "";
+      switch (operation) {
+        case "+":
+          this.add();
+          break;
+        case "-":
+          this.minus();
+          break;
+        case "*":
+          this.multiplication();
+          break;
+        case "/":
+          this.divide();
+          break;
+      }
+      const key = Date.now();
+      const value = `${this.operand1}${operation}${this.operand2}=${this.result}`;
+      Vue.set(this.logs, key, value);
+      // this.logs[
+      //   Date.now()
+      // ] = `${this.operand1}${operation}${this.operand2}=${this.result}`;
     },
   },
 };
